8f17638ddc8c16f427a5156b1ef618146316f2b5
Sourav Choudhury <62167988+SouravCodes101@users.noreply.github.com>
2022-11-13 16:19:06 +0530

    added Link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./footer.css";
 
 const Footer = () => {
   return (
     <div className="footer__container">
       <p className="footer__container-item">
-        <a href="/friends/enrolled">Friends Who Enrolled</a>
+        <Link to="/friends/enrolled">Friends Who Enrolled</Link>
       </p>
       <p className="footer__container-item">
         <a href="#">Terms & Conditions</a>
